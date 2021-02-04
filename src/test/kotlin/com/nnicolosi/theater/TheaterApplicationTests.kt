1015e7905189c5cfcae5abed306750e793524ff8
Nick Nicolosi <nnicolosi@singlestoneconsulting.com>
2021-02-04 09:10:06 -0500

    Abandon spring-boot-starter-test in favor of JUnit 5, MockK, and Kotest

--- src/test/kotlin/com/nnicolosi/theater/TheaterApplicationTests.kt
+++ src/test/kotlin/com/nnicolosi/theater/TheaterApplicationTests.kt
@@ -1,13 +1,13 @@
 package com.nnicolosi.theater
 
 import org.junit.jupiter.api.Test
-import org.springframework.boot.test.context.SpringBootTest
+import org.junit.jupiter.api.TestInstance
 
-@SpringBootTest
+@TestInstance(TestInstance.Lifecycle.PER_CLASS)
 class TheaterApplicationTests {
 
     @Test
     fun contextLoads() {
+        assert(true).equals(true)
     }
-
 }
